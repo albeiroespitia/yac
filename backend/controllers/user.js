316cb93d8450430ebbf6c75f270e06e974389abb
Albeiro <albeiroespitiasierra@gmail.com>
2020-01-24 04:30:44 -0500

    Create login endpoint

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -10,13 +10,29 @@ async function signup (req, res) {
 
 	try{
 		const docRef = await db.collection('users').doc().set(user)
-		console.log(docRef)
 		res.status(200).send(docRef)
 	}catch(e){
 		res.status(500).send(e)
 	}
 }
 
+async function login (req, res) {
+  const user = {
+    nickname: req.body.nickname,
+  }
+
+	try{
+		const querySnapshot = await db.collection('users').where("nickname", "==", user.nickname).get()
+		if(querySnapshot.empty){
+			res.status(401).send("Empty")
+		}
+		res.status(200).send(querySnapshot.docs[0].data())
+	}catch(e){
+		res.status(500).send(e)
+	}
+}
+
 module.exports = {
-	signup
+	signup,
+	login
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,5 +3,6 @@ const userCtrl = require('../controllers/user')
 const api = express.Router()
 
 api.post('/signup', userCtrl.signup)
+api.post('/login', userCtrl.login)
 
 module.exports = api
