87de1ca9c75d37084111408d7d1bd06481bcb763
Albeiro <albeiroespitiasierra@gmail.com>
2020-01-25 06:43:34 -0500

    Custom scroll bar for chatbox

--- frontend/src/Components/MessageBox/index.js
+++ frontend/src/Components/MessageBox/index.js
@@ -2,19 +2,36 @@ import React from 'react'
 import styles from './style.module.scss'
 import {TextField, Typography} from '@material-ui/core'
 
-const MessageBox = () => {
+const MessageBox = ({test}) => {
 
 	return(
-		<div className={styles.container}>
-			<img src={require('../../assets/avatars/boy.svg')}/>
-			<div className={styles.messageContainer}>
-				<Typography component="span" className={styles.person}>Albeiro</Typography>
-				<div className={styles.textContainer}>
-					<Typography component="p" className={styles.message}>Hi Star. How is it going? :D</Typography>
-				</div>
-				<Typography component="span" className={styles.dateMessage}>Yesterday 12:55 AM</Typography>
-			</div>
-		</div>
+		<>
+			{
+				test ? (
+					<div className={styles.container} style={{float:'right', justifyContent:'flex-end'}}>
+						<div className={styles.messageContainer} style={{alignItems:'flex-end'}}>
+							<Typography component="span" className={styles.person}>Albeiro</Typography>
+							<div className={styles.textContainer} style={{backgroundColor:'#7E00C5'}}>
+								<Typography component="p" className={styles.message} style={{color:'#FFF'}}>Hi Star. How is it going? :D</Typography>
+							</div>
+							<Typography component="span" className={styles.dateMessage} style={{alignSelf:'flex-start'}}>Yesterday 12:55 AM</Typography>
+						</div>
+						<img src={require('../../assets/avatars/boy.svg')}/>
+					</div>
+				):(
+					<div className={styles.container}>
+						<img src={require('../../assets/avatars/boy.svg')}/>
+						<div className={styles.messageContainer}>
+							<Typography component="span" className={styles.person}>Albeiro</Typography>
+							<div className={styles.textContainer}>
+								<Typography component="p" className={styles.message}>Hi Star. How is it going? :D</Typography>
+							</div>
+							<Typography component="span" className={styles.dateMessage}>Yesterday 12:55 AM</Typography>
+						</div>
+					</div>
+				)
+			}
+		</>
 	)
 }
 
--- frontend/src/Pages/Chat/index.js
+++ frontend/src/Pages/Chat/index.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import Layout from '../../Layout'
 import styles from './style.module.scss'
 import InputBar from '../../Components/InputBar'
 import MessageBox from '../../Components/MessageBox'
 
 const Chat = () => {
+	const scrollDiv = useRef()
+
+	useEffect(()=>{
+		scrollDiv.current.scrollTop = scrollDiv.current.scrollHeight;
+	},[])
+
 	return(
 		<Layout>
 			<div className={styles.chatContainer}>
-				<div className={styles.chatBox}>
-					<MessageBox/>
+				<div className={styles.chatBox} ref={scrollDiv}>
+					<MessageBox test={false}/>
+					<MessageBox test={true}/>
+					<MessageBox test={false}/>
+					<MessageBox test={false}/>
+					<MessageBox test={false}/>
+					<MessageBox test={true}/>
+					<MessageBox test={true}/>
 				</div>
 				<div className={styles.chatInput}>
 					<InputBar/>
