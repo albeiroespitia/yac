1bd73a668c4c85b4ca289a6e6266a14d36282cb3
Albeiro <albeiroespitiasierra@gmail.com>
2020-01-25 18:54:38 -0500

    Add youtube bot with youtube api v3

--- frontend/src/Components/MessageBox/index.js
+++ frontend/src/Components/MessageBox/index.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styles from './style.module.scss'
 import {TextField, Typography} from '@material-ui/core'
 import { useSelector } from 'react-redux'
 import moment from 'moment'
+import searchYoutube from 'youtube-api-v3-search';
 
 const MessageBox = ({test, message, sender, date}) => {
 	const user = useSelector(state => state.user)
+	const [formattedMessage, setFormattedMessage] = useState(message)
 	const formattedDate = moment(date).calendar();
+
+	const youtubeAPI = async() => {
+		if(/^\/youtube\s\S.*$/.test(message)){
+			try{
+				const result = await searchYoutube(process.env.REACT_APP_YOUTUBE_API,{
+					"q": message,
+					"part": "snippet",
+					"maxResults": 1,
+					"type": "video",
+					"videoEmbeddable": "true"
+				});
+				setFormattedMessage(<iframe allowfullscreen="allowfullscreen" src={`https://www.youtube.com/embed/${result.items[0].id.videoId}`}></iframe>)
+			}catch(e){
+				console.log(e)
+			}
+		}
+	}
+
+	useEffect(()=>{
+		youtubeAPI()
+	},[])
 
 	return(
 		<>
@@ -16,7 +39,7 @@ const MessageBox = ({test, message, sender, date}) => {
 						<div className={styles.messageContainer} style={{alignItems:'flex-end'}}>
 							<Typography component="span" className={styles.person}>{sender}</Typography>
 							<div className={styles.textContainer} style={{backgroundColor:'#7E00C5'}}>
-								<Typography component="p" className={styles.message} style={{color:'#FFF'}}>{message}</Typography>
+								<Typography component="p" className={styles.message} style={{color:'#FFF'}}>{formattedMessage}</Typography>
 							</div>
 							<Typography component="span" className={styles.dateMessage} style={{alignSelf:'flex-start'}}>{formattedDate}</Typography>
 						</div>
@@ -28,7 +51,7 @@ const MessageBox = ({test, message, sender, date}) => {
 						<div className={styles.messageContainer}>
 							<Typography component="span" className={styles.person}>{sender}</Typography>
 							<div className={styles.textContainer}>
-								<Typography component="p" className={styles.message}>{message}</Typography>
+								<Typography component="p" className={styles.message}>{formattedMessage}</Typography>
 							</div>
 							<Typography component="span" className={styles.dateMessage}>{formattedDate}</Typography>
 						</div>
